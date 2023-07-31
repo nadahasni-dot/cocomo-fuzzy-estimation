dc3c292b3f0fc829b6859cc1161d6ddfd01a56f3
Nada Hasni <nadasthing@gmail.com>
2023-07-31 19:45:46 +0700

    feat: projects list

--- resources/js/Pages/Projects/index.jsx
+++ resources/js/Pages/Projects/index.jsx
@@ -1,5 +1,6 @@
+import ProjectCard from "@/Components/Project/ProjectCard";
 import Authenticated from "@/Layouts/Authenticated";
-import { Head } from "@inertiajs/inertia-react";
+import { Head, Link } from "@inertiajs/inertia-react";
 import React from "react";
 
 export default function Projects(props) {
@@ -16,7 +17,97 @@ export default function Projects(props) {
             <Head title="Projects" />
 
             <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8"></div>
+                <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="flex justify-end">
+                        <Link href="#" className="px-3 py-1 text-sm font-bold transition bg-indigo-200 rounded-lg shadow hover:bg-indigo-100">+ Add Project</Link>
+                    </div>
+                    <div className="grid grid-cols-1 gap-1 mt-4 md:gap-6 md:grid-cols-2 lg:grid-cols-4">
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                        <ProjectCard
+                            name="Test Project"
+                            ksloc={2000}
+                            time={5}
+                            people={6}
+                            cost={1000000}
+                        />
+                    </div>
+                </div>
             </div>
         </Authenticated>
     );
