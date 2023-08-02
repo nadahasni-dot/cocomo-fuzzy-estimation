bd751d60134e325d0c61332a2b59c124c8e44143
Nada Hasni <nadasthing@gmail.com>
2023-08-02 11:19:28 +0700

    feat: project model migration relation

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'image' => null,
+        ];
+    }
+}
