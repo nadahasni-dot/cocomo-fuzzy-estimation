a8e8aa5e0252893eb91f94e33b93c257f39b95fa
Nada Hasni <nadasthing@gmail.com>
2023-08-14 12:51:10 +0700

    feat: change guest template logo

--- resources/js/Layouts/Guest.jsx
+++ resources/js/Layouts/Guest.jsx
@@ -7,7 +7,7 @@ export default function Guest({ children }) {
         <div className="flex flex-col items-center w-full min-h-screen pt-6 bg-grey-100 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-20 h-20" />
+                    <ApplicationLogo className="w-64" />
                 </Link>
             </div>
 
